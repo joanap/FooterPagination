5b9582ea1d6bc56e32919ac0e976e05c1d5a5a11
Joana <joana.pinto@xpand-it.com>
2018-01-28 17:35:47 +0000

    [UPD] Compute initial status of around and end bins.

--- src/footer_pagination.py
+++ src/footer_pagination.py
@@ -19,6 +19,41 @@ def init_beginning_pages(total_pages, boundaries):
     else:
         None
 
+
+def init_end_pages(total_pages, boundaries):
+    """
+
+    :param total_pages:
+    :param boundaries:
+    :return: first end page, last end page
+    """
+    if boundaries != 0:
+        if total_pages - boundaries > 0:
+            return total_pages - boundaries+1, total_pages
+        else:
+            return FIRST_PAGE, total_pages
+    else:
+        return None
+
+
+def init_around_pages(current_page, around, total_pages):
+    """
+
+    :param current_page:
+    :param around:
+    :param total_pages:
+    :return: first around page, last around page
+    """
+    around_first_page = around_last_page = current_page
+    if around != 0:
+        around_first_page = current_page - around
+        around_last_page = current_page + around
+        if around_first_page < 1:
+            around_first_page = 1
+        if around_last_page > total_pages:
+            around_last_page = total_pages
+    return around_first_page, around_last_page
+
 def main():
 
     if len(sys.argv) == INPUT_LEN:
