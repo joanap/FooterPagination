f9d5b4754a94baa8c8ffe5ff8d0e54578f0f6a87
Joana <joana.pinto@xpand-it.com>
2018-01-28 20:34:06 +0000

    [UPD] Methods to check and update sets of pages if they overlap.

--- src/footer_pagination.py
+++ src/footer_pagination.py
@@ -2,6 +2,8 @@ import sys
 
 INPUT_LEN = 5
 FIRST_PAGE = 1
+FIRST_PAGE_INDEX = 0
+LAST_PAGE_INDEX = 1
 
 
 def init_beginning_pages(total_pages, boundaries):
@@ -49,10 +51,62 @@ def init_around_pages(current_page, around, total_pages):
         around_first_page = current_page - around
         around_last_page = current_page + around
         if around_first_page < 1:
-            around_first_page = 1
+            around_first_page = FIRST_PAGE
         if around_last_page > total_pages:
             around_last_page = total_pages
     return around_first_page, around_last_page
+
+
+def initial_pages_status(current_page, total_pages, boundaries, around):
+    """
+
+    :param current_page:
+    :param total_pages:
+    :param boundaries:
+    :param around:
+    :return:
+    """
+    beginning_pages = init_beginning_pages(total_pages, boundaries)
+    around_pages = init_around_pages(current_page, around, total_pages)
+    end_pages = init_end_pages(total_pages, boundaries)
+    return [beginning_pages, around_pages, end_pages]
+
+
+def are_overlapping_pages(pages1, pages2):
+    """Does the range (start1, end1) overlap with (start2, end2)?
+    :param pages1:
+    :param pages2:
+    :return:
+    """
+    if pages1 is None or pages2 is None:
+        return False
+    else:
+        return not (pages1[LAST_PAGE_INDEX] < pages2[FIRST_PAGE_INDEX] or
+                    pages2[LAST_PAGE_INDEX] < pages1[FIRST_PAGE_INDEX])
+
+
+def unify_pages(pages1, pages2):
+    """
+    Merge two overlapping sets of pages
+    :param pages1:
+    :param pages2:
+    :return:
+    """
+    return min(pages1[FIRST_PAGE_INDEX], pages2[FIRST_PAGE_INDEX]), max(pages1[LAST_PAGE_INDEX], pages2[LAST_PAGE_INDEX])
+
+
+def update_pages(pages1, pages2):
+    """
+    Merge two sets of pages if they overlap, otherwise returns the initial sets
+    :param pages1:
+    :param pages2:
+    :return:
+    """
+    if are_overlapping_pages(pages2, pages1):
+        return unify_pages(pages2, pages1), None
+    else:
+        pages1, pages2
+
 
 def main():
 
@@ -67,7 +121,7 @@ def main():
         boundaries = 1
         around = 0
 
-        # print("Missing arguments")
+    # print("Missing arguments")
 
 
 if __name__ == '__main__':
