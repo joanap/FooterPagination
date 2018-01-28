2f15fdf6ae4bf208814dcfbd7b0c7297243f5a7f
Joana <joana.pinto@xpand-it.com>
2018-01-28 17:36:11 +0000

    [UPD] Tests for initial status of around and end bins.

--- tests/simple_tests.py
+++ tests/simple_tests.py
@@ -10,6 +10,18 @@ class SimpleTests(unittest.TestCase):
         """
         self.assertSequenceEqual((1, 1), footer_pagination.init_beginning_pages(5, 1))
 
+    def test_end_pages(self):
+        """
+        Test the initial status of the end pages bin
+        """
+        self.assertSequenceEqual((5, 5), footer_pagination.init_end_pages(5, 1))
+
+    def test_around_pages(self):
+        """
+        Test the initial status of the around pages bin
+        """
+        self.assertSequenceEqual((4, 4), footer_pagination.init_around_pages(4, 0, 5))
+
 
 def main():
     unittest.main()
