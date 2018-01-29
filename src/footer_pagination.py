54f1dad9fd21c397234ee02e69d411d032a91ca4
Joana <joana.pinto@xpand-it.com>
2018-01-29 07:20:08 +0000

    [UPD] Methods to compute footer pagination.

--- src/footer_pagination.py
+++ src/footer_pagination.py
@@ -4,14 +4,15 @@ INPUT_LEN = 5
 FIRST_PAGE = 1
 FIRST_PAGE_INDEX = 0
 LAST_PAGE_INDEX = 1
+REMAINING_PAGES = "..."
 
 
 def init_beginning_pages(total_pages, boundaries):
-    """
+    """Define the initial status for the set of pages in the beginning: return first and last page
 
-    :param total_pages:
-    :param boundaries:
-    :return: first beginning page, last beginning page
+    :param total_pages: total number of pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    :return: (first beginning page, last beginning page)
     """
     if boundaries != 0:
         if boundaries < total_pages:
@@ -23,11 +24,11 @@ def init_beginning_pages(total_pages, boundaries):
 
 
 def init_end_pages(total_pages, boundaries):
-    """
+    """Define the initial status for the set of pages in the end: return first and last page
 
-    :param total_pages:
-    :param boundaries:
-    :return: first end page, last end page
+    :param total_pages: total number of pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    :return: (first end page, last end page)
     """
     if boundaries != 0:
         if total_pages - boundaries > 0:
@@ -39,12 +40,12 @@ def init_end_pages(total_pages, boundaries):
 
 
 def init_around_pages(current_page, around, total_pages):
-    """
+    """Define the initial status for the set of pages in the around: return first and last page
 
-    :param current_page:
-    :param around:
-    :param total_pages:
-    :return: first around page, last around page
+    :param current_page: current page
+    :param around: how many pages we want to link before and after the current page
+    :param total_pages: total number of pages
+    :return: (first around page, last around page)
     """
     around_first_page = around_last_page = current_page
     if around != 0:
@@ -58,13 +59,13 @@ def init_around_pages(current_page, around, total_pages):
 
 
 def initial_pages_status(current_page, total_pages, boundaries, around):
-    """
+    """Define the initial status for the sets of pages: return a list with beginning, around and end set of pages
 
-    :param current_page:
-    :param total_pages:
-    :param boundaries:
-    :param around:
-    :return:
+    :param current_page: current page
+    :param total_pages: total number of pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    :param around: how many pages we want to link before and after the current page
+    :return: list with beginning, around and end set of pages
     """
     beginning_pages = init_beginning_pages(total_pages, boundaries)
     around_pages = init_around_pages(current_page, around, total_pages)
@@ -73,10 +74,11 @@ def initial_pages_status(current_page, total_pages, boundaries, around):
 
 
 def are_overlapping_pages(pages1, pages2):
-    """Does the range (start1, end1) overlap with (start2, end2)?
-    :param pages1:
-    :param pages2:
-    :return:
+    """Check if the sets pages1 and pages2 overlap: return True if pages1 and pages2 overlap
+
+    :param pages1: set of pages
+    :param pages2: set of pages
+    :return: True if pages1 and pages2 overlap
     """
     if pages1 is None or pages2 is None:
         return False
@@ -85,43 +87,159 @@ def are_overlapping_pages(pages1, pages2):
                     pages2[LAST_PAGE_INDEX] < pages1[FIRST_PAGE_INDEX])
 
 
-def unify_pages(pages1, pages2):
-    """
-    Merge two overlapping sets of pages
-    :param pages1:
-    :param pages2:
-    :return:
+def merge_pages(pages1, pages2):
+    """Merge overlapping sets of pages1 and pages2: return the merged set of pages
+
+    :param pages1: set of pages
+    :param pages2: set of pages
+    :return: merged set of pages
     """
     return min(pages1[FIRST_PAGE_INDEX], pages2[FIRST_PAGE_INDEX]), max(pages1[LAST_PAGE_INDEX], pages2[LAST_PAGE_INDEX])
 
 
 def update_pages(pages1, pages2):
-    """
-    Merge two sets of pages if they overlap, otherwise returns the initial sets
-    :param pages1:
-    :param pages2:
-    :return:
+    """Merge two sets of pages if they overlap, otherwise return the initial status of the sets
+
+    :param pages1: set of pages
+    :param pages2: set of pages
+    :return: (merged set of pages, None) if pages1 and pages2 overlap, otherwise return (pages1, pages2)
     """
     if are_overlapping_pages(pages2, pages1):
-        return unify_pages(pages2, pages1), None
+        return merge_pages(pages2, pages1), None
     else:
-        pages1, pages2
+        return pages1, pages2
+
+
+def update_all_pages(initial_pages_status):
+    """Iterate the sets of pages and check if the current set of pages overlap the next sets of pages; unify sets
+    that overlap.
+
+    :param initial_pages_status: initial pages status
+    :return: final pages status with no overlapping.
+    """
+
+    for pages_index, item in enumerate(initial_pages_status):
+        for i in range(pages_index, len(initial_pages_status) - 1):
+            new_pages_status = update_pages(initial_pages_status[pages_index], initial_pages_status[i+1])
+            if new_pages_status is not None:
+                if initial_pages_status[pages_index] is not None:
+                    initial_pages_status[pages_index] = new_pages_status[0]
+                if initial_pages_status[i+1] is not None:
+                    initial_pages_status[i+1] = new_pages_status[1]
+    return initial_pages_status
+
+
+def exist_remaining_pages(pages1, pages2):
+    """Check if there are remaining pages between the sets of pages pages1 and pages2
+
+    :param pages1: set of pages
+    :param pages2: set of pages
+    :return: True if exist remaining pages between pages1 and pages2
+    """
+    if pages1 is not None and pages2 is not None:
+        return pages2[FIRST_PAGE_INDEX] - pages1[LAST_PAGE_INDEX] > 1
+    else:
+        return False
+
+
+def print_range(pages):
+    """Print the range of pages in the set pages
+
+    :param pages: set of pages to print
+    """
+    if pages is not None:
+        print(*range(pages[FIRST_PAGE_INDEX], pages[LAST_PAGE_INDEX]+1), sep=' ', end='')
+
+
+def find_page(pages_list, page_to_found):
+    """Check if page_to_found is in pages_list: return True if exists
+
+    :param pages_list: list with sets of pages
+    :param page_to_found: page to found in the list
+    :return: True if the page is in the list
+    """
+    for current_pages in pages_list:
+        if current_pages is not None:
+            if page_to_found == current_pages[FIRST_PAGE_INDEX] or page_to_found == current_pages[LAST_PAGE_INDEX]:
+                return True
+    return False
+
+
+def remove_none(pages_list):
+    """Remove None elements from a list
+
+    :param pages_list: list of sets of pages
+    :return: list without None elements
+    """
+    return [pages for pages in pages_list if pages is not None]
+
+
+def print_output(pages_list, last_page, boundaries):
+    """Concatenate and print footer pagination
+
+    :param pages_list: sets of pages
+    :param last_page: total pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    """
+    pages_list_without_none = remove_none(pages_list)
+    if boundaries == 0 and not find_page(pages_list_without_none, FIRST_PAGE):
+        print(REMAINING_PAGES + " ", end='')
+    for pages_index, current_pages in enumerate(pages_list_without_none):
+        print_range(current_pages)
+        if pages_index + 1 < len(pages_list_without_none):
+            if exist_remaining_pages(current_pages, pages_list_without_none[pages_index + 1]):
+                print(" " + REMAINING_PAGES + " ", end='')
+            else:
+                print(" ", end='')
+    if boundaries == 0 and not find_page(pages_list_without_none, last_page):
+        print(" " + REMAINING_PAGES, end='')
+
+
+def validate_input(current_page, total_pages, boundaries, around):
+    """
+    Raises an exception if input is invalid
+    :param current_page: current page
+    :param total_pages: total number of pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    :param around: how many pages we want to link before and after the current page
+    """
+    if current_page <= 0 or total_pages <= 0:
+        raise ValueError("Current page and total pages must be greater than 0")
+    if boundaries < 0 or around < 0:
+        raise ValueError("Boundaries and around must be greater or equal to 0")
+    if current_page > total_pages:
+        raise ValueError("Current page must be lower than total pages")
+
+
+def get_footer_pagination(current_page, total_pages, boundaries, around):
+    """Build and print footer pagination according page, total_pages, boundaries and around
+
+    :param current_page: current page
+    :param total_pages: total number of pages
+    :param boundaries: how many pages we want to link in the beginning, or end
+    :param around: how many pages we want to link before and after the current page
+    """
+    initial_pages_stat = initial_pages_status(current_page, total_pages, boundaries, around)
+    final_pages_stat = update_all_pages(initial_pages_stat)
+    print_output(final_pages_stat, total_pages, boundaries)
 
 
 def main():
+    """Read arguments current_page, total_page, boundaries and around and build the corresponding footer pagination
 
+    """
     if len(sys.argv) == INPUT_LEN:
         current_page = int(sys.argv[1])
         total_pages = int(sys.argv[2])
         boundaries = int(sys.argv[3])
         around = int(sys.argv[4])
+        try:
+            validate_input(current_page, total_pages, boundaries, around)
+            get_footer_pagination(current_page, total_pages, boundaries, around)
+        except ValueError as err:
+            print(err)
     else:
-        current_page = 4
-        total_pages = 5
-        boundaries = 1
-        around = 0
-
-    # print("Missing arguments")
+        print("Missing arguments")
 
 
 if __name__ == '__main__':
